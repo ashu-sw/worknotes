e9821da4783969ca179212aea4ed6b049e859d2d
ashutosh <stayintune.gmail.com>
2021-10-23 22:39:46 +0530

    work note react app code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Content from './components/content.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+        <Content />
+      </main>
+      <footer class="App-logo">
+        @Ashutosh
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Content = (props) => {
+    const welcomeMsg = 'Welcome to work Notes';
+    return (
+        <div>
+            <h2>{welcomeMsg}</h2>
+        </div>
+    )
+}
+
+export default Content;
+
